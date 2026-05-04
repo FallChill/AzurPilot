11ca23f9ad2ae8ec5912f45d2e9dcd9b416156be
wess09 <zicheng_1202@outlook.com>
2026-05-04 21:09:36 +0800

    f

--- deploy/config.py
+++ deploy/config.py
@@ -1,4 +1,7 @@
 import copy
+import os
+import subprocess
+import sys
 from typing import Optional, Union
 
 from deploy.logger import logger
@@ -181,7 +184,10 @@ class DeployConfig(ConfigModel):
         if not output:
             command = command + ' >nul 2>nul'
         logger.info(command)
-        error_code = os.system(command)
+        if sys.platform == 'win32':
+            error_code = os.system(f'"{command}"')
+        else:
+            error_code = os.system(command)
         if error_code:
             if allow_failure:
                 logger.info(f"[ allowed failure ], error_code: {error_code}")
