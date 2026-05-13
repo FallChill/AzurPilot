7694da9f60bff70e4286d0152d7549014cff9551
wess09 <zicheng_1202@outlook.com>
2026-05-13 10:55:37 +0800

    fix

--- deploy/git.py
+++ deploy/git.py
@@ -106,9 +106,12 @@ class GitManager(DeployConfig):
         if data is True or (isinstance(data, str) and data.lower() in ('true', 'ture')):
             logger.info('Cloud update control is enabled')
             return True
+        if data is False or (isinstance(data, str) and data.lower() in ('false', 'fales')):
+            logger.info('Cloud update control is disabled')
+            return False
 
-        logger.info(f'Cloud update control is disabled: {text}')
-        return False
+        logger.info(f'Cloud update control is inaccessible: {text}')
+        return None
 
     def cloud_update_access_failed(self):
         logger.hr('Cloud Update Control Failed', 0)
